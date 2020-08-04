7a2cffd4d98c48527b5f52e82caaf495ca6d3920
Eagle19243 <eagle19243@gmail.com>
2020-08-04 20:21:40 +0300

    remove bower.json and update page title

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 import Admin from "layouts/Admin.js";
 import "assets/css/material-dashboard-react.css?v=1.9.0";
 
-const hist = createBrowserHistory();
+const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={hist}>
+  <Router history={history}>
     <Switch>
       <Route path="/admin" component={Admin} />
       <Redirect from="/" to="/admin/dashboard" />
