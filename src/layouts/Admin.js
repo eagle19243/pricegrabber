5221c464ccae4006d601821f566d726e38bd40f7
Eagle19243 <eagle19243@gmail.com>
2020-08-04 21:20:16 +0300

    remove search input and notifications

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -83,7 +83,7 @@ export default function Admin({ ...rest }) {
     <div className={classes.wrapper}>
       <Sidebar
         routes={routes}
-        logoText={"Creative Tim"}
+        logoText={"Price Grabber"}
         logo={logo}
         image={image}
         handleDrawerToggle={handleDrawerToggle}
