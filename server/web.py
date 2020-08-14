022cd5cf29193d4f6c265c17cd76f6a662dc11e3
Eagle19243 <eagle19243@gmail.com>
2020-08-14 16:35:43 +0300

    fix url mapping

--- server/web.py
+++ server/web.py
@@ -13,7 +13,7 @@ from .scheduler import Scheduler
 
 config = load_config()
 
-APP = Flask(__name__, static_folder='../build', static_url_path='/')
+APP = Flask(__name__, static_folder='../build')
 APP.config['JWT_SECRET_KEY'] = config['SECRET_KEY']
 APP.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
 APP.config.from_mapping(
@@ -46,7 +46,8 @@ def unauthorized_response(callback):
 
 @APP.route('/', defaults={'path': ''})
 @APP.route('/<path:path>')
-def index(path=None):
+def index(path):
+    print(path)
     return send_from_directory(APP.static_folder, 'index.html')
 
 
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const Config = {
-  api_url: 'http://127.0.0.1:5000/'
+  api_url: 'http://127.0.0.1:8093/'
 };
 
 export default Config;
