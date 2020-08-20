9b4cf629373aef9852f0bb32887f78d7c166e320
Eagle19243 <eagle19243@gmail.com>
2020-08-20 12:07:22 +0300

    update api url in config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const apiURL = process.env.NODE_ENV === 'development' ? 
                 'http://127.0.0.1:5000/' :
-                'http://127.0.0.1:8093/';
+                'http://116.203.78.255:8093/';
                 
 const Config = {
   apiURL: apiURL
