e167a48b9add2a4dfc309f832b84f7b43ab5a479
Eagle19243 <eagle19243@gmail.com>
2020-08-22 06:32:41 +0300

    add dependency, change log file path

--- run_celery.sh
+++ run_celery.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-celery -A server.celery.celery worker -B -l INFO --logfile="/tmp/pricegrabber.log"
+celery -A server.celery.celery worker -B -l INFO --logfile="~/pricegrabber.log"
